0c691df917582a581eb814d8d75942850ebb4c8f
Gileadepe <gil_eade@hotmail.com>
2022-02-07 16:33:58 -0300

    Atualização do arquivo js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,60 @@
-var nome = "Gileade Cavalcante";
-var n1 = 8;
-var n2 = 7;
-var frase = "Alemanha é o melhor time do mundo";
+var d = new Date();
+alert(d.getDay());
+
+/*
+var count;
+for(count = 0; count <= 5; count ++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count = count +1;
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade:");
+if(idade >= 18){
+    alert("Maior de Idade");
+}else{
+    alert("Menor de Idade");
+}
+*/
+
+/*
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"verde"}];
+console.log(frutas);
+console.log(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
+*/
+
+//var lista = ["maçã","pêra","laranja"];
+//lista.push("uva");
+
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista[0]);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+//var nome = "Gileade Cavalcante";
+//var n1 = 8;
+//var n2 = 7;
+//var frase = "Alemanha é o melhor time do mundo";
 //alert(nome + " Tem "+ idade + " anos");
 //alert(idade + idade2);
-console.log(nome);
-console.log(n1*n2);
-console.log(frase.toLocaleLowerCase());
+//console.log(nome);
+//console.log(n1*n2);
+//console.log(frase.toLocaleLowerCase());
 //alert(frase.replace("Alemanha", "Brasil"));
